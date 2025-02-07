1434ecf23a2654025d86188fb5205e7a74b225d3
ozewr <94829827+ozewr@users.noreply.github.com>
2025-02-07 14:37:27 +0800

    feat: re-export prost_types (#6)
    * feat: re-export prost_types
    
    * Update src/lib.rs
    
    Co-authored-by: shuiyisong <113876041+shuiyisong@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: ozewr <l19ht@google.com>
    Co-authored-by: shuiyisong <113876041+shuiyisong@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub use prost_types;
 pub mod logproto {
     tonic::include_proto!("logproto");
 }
